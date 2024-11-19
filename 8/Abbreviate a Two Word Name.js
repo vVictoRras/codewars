9c532238fbfa39a67ed54424d09a6c7aa0a16116
Victor <you@example.com>
2024-11-19 20:57:55 +0200

    moved task in folder 8

--- 8/Abbreviate a Two Word Name.js
+++
@@ -1,7 +0,0 @@
-function abbrevName(name) {
-   const arr = name.split(' ');
-   const firstname = arr[0];
-   const surname = arr[1];
-   const result = firstname[0].toUpperCase() + '.' + surname[0].toUpperCase();
-   return result;
- }
