0d385e55c4570baf7b030394c1b3a311b3edc362
Victor <you@example.com>
2024-11-19 21:30:45 +0200

    task  8 Max and Min

--- 8/Calculate average.js
+++ 8/Calculate average.js
@@ -1,3 +1,9 @@
+/*let array=[6,8,10]
 function find_average(array) {
-    return array.reduce((result, current) => result + current, 0) / array.length;
+    const result = array.reduce((a, b) => a + b, 0)
+    const totalItems = array.length
+    return (result / totalItems)
 }
+console.log(find_average(array))*/
+
+const findAverage = array => array.reduce((a, b) => a + b, 0) / array.length
