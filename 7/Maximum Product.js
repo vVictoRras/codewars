e6719044c51c12e895d97be0d3a988ee9a528437
Victor <you@example.com>
2024-12-04 19:42:26 +0200

    completed

--- 7/Maximum Product.js
+++ 7/Maximum Product.js
@@ -1,3 +1,9 @@
-function adjacentElementsProduct(array) {
-    return 0
+function adjacentElementsProduct(x) {
+    let max = x[0]*x[1]
+    for(let i=1; i<x.length-1;i++){
+        if(x[i]*x[i+1]>max){
+            max=x[i]*x[i+1]
+        }
+    }
+    return max
 }
