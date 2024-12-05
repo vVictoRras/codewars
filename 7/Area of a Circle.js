a43f56936631bacf6b89a23ecccca84282447d3f
Victor <you@example.com>
2024-12-05 21:48:07 +0200

    edited

--- 7/Area of a Circle.js
+++ 7/Area of a Circle.js
@@ -1,4 +1,8 @@
-var circleArea = function(radius) {
-    if (radius<=0||typeof radius !== 'number') return false;
+let circleArea = function(radius) {
+    if (radius<=0||typeof radius !== 'number')
+        throw('error')
+    else
     return +((Math.PI*radius*radius).toFixed(2))
 };
+
+console.log(circleArea(10))
