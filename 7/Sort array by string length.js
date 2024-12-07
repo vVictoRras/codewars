f329586537b88d4f0a653fd24ed783095111dc77
Victor <you@example.com>
2024-12-07 19:36:36 +0200

    bingo!!!!!

---
+++ 7/Sort array by string length.js
@@ -0,0 +1,8 @@
+
+    function sortByLength (array) {
+
+        const arr1 = array.sort((a, b) => a.length - b.length)
+       return (arr1)
+
+    }
+    console.log(sortByLength ( [ 'Beg', 'Life', 'I', 'To' ]))
